5e3c2753b699a0c5977caa540b8b7ff83171d8a3
Òscar Casajuana <elboletaire@gmail.com>
2024-02-11 23:55:58 +0100

    Add BASE_URL and BUILD_PATH env vars to webapp

--- fc-create-election/vite.config.ts
+++ fc-create-election/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+const base = process.env.BASE_URL || '/'
+const outDir = process.env.BUILD_PATH || 'dist'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base,
+  build: {
+    outDir,
+  },
   plugins: [react()],
 })
