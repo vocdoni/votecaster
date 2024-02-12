9e97557ae70e2398777aa5b2a0eb596589b12768
Òscar Casajuana <elboletaire@gmail.com>
2024-02-12 01:33:35 +0100

    Set proper poll url

--- fc-create-election/src/Form.tsx
+++ fc-create-election/src/Form.tsx
@@ -85,8 +85,8 @@ const Form: React.FC = () => {
               <Text display='inline'>
                 Done! You can now share it using this link:
                 <br />
-                <Link href={`${appUrl}/${pid}`}>
-                  {appUrl}/{pid}
+                <Link href={`${appUrl}/poll/${pid}`}>
+                  {appUrl}/poll/{pid}
                 </Link>
               </Text>
             ) : (
