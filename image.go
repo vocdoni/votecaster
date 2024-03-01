ab0bc97d83c5393f65b22569c9b4f4652e3ab136
p4u <pau@dabax.net>
2024-03-01 22:47:32 +0100

    fixes for PNG backwards compatibility
    Signed-off-by: p4u <pau@dabax.net>

--- image.go
+++ image.go
@@ -15,6 +15,10 @@ import (
 	"lukechampine.com/blake3"
 )
 
+var oldImagesHandlerMap = map[string]string{
+	"08d56a13ee330b927b7d181a3ee17de580e265ca51ac5b0728c2a272712585bd": "4ae20a8eb4caa52f5588f7bb9f3c6d6b7cf003a5b03f4589edea1000000000a2",
+}
+
 func (v *vocdoniHandler) imagesHandler(msg *apirest.APIdata, ctx *httprouter.HTTPContext) error {
 	id := ctx.URLParam("id")
 	data := v.imageFromCache(id)
@@ -22,18 +26,42 @@ func (v *vocdoniHandler) imagesHandler(msg *apirest.APIdata, ctx *httprouter.HTT
 		return imageResponse(ctx, data)
 	}
 	idSplit := strings.Split(id, "_")
+	var electionID types.HexBytes
+	var err error
 	if len(idSplit) != 2 {
-		log.Debugw("access to old PNG", "requestURI", ctx.Request.RequestURI, "url", ctx.Request.URL, "headers", ctx.Request.Header)
-		return errorImageResponse(ctx, fmt.Errorf("nothing here... click results"))
+		// for backwards compatibility, check if the id is in the oldImagesHandlerMap
+		// remove this code after some weeks
+		if eid, ok := oldImagesHandlerMap[id]; ok {
+			log.Warnw("old PNG match", "id", id)
+			electionID, err = hex.DecodeString(eid)
+			if err != nil {
+				return errorImageResponse(ctx, fmt.Errorf("nothing here... click results"))
+			}
+			election, err := v.election(electionID)
+			if err != nil {
+				return errorImageResponse(ctx, fmt.Errorf("id not found... click results"))
+			}
+			png, err := buildResultsPNG(election)
+			if err != nil {
+				return errorImageResponse(ctx, fmt.Errorf("failed to build results: %w", err))
+			}
+			return imageResponse(ctx, png)
+		} else {
+			log.Debugw("access to old PNG", "requestURI", ctx.Request.RequestURI, "url", ctx.Request.URL)
+			return errorImageResponse(ctx, fmt.Errorf("nothing here... click results"))
+		}
 	}
-	electionID, err := hex.DecodeString(idSplit[0])
+
+	electionID, err = hex.DecodeString(idSplit[0])
 	if err != nil {
 		return errorImageResponse(ctx, fmt.Errorf("failed to decode id: %w", err))
 	}
 	// check if the election is finished and if so, send the final results as a static PNG
-	if v.checkIfElectionFinishedAndHandle(electionID, ctx) {
-		return nil
+	pngResults := v.db.FinalResultsPNG(electionID)
+	if pngResults != nil {
+		return imageResponse(ctx, pngResults)
 	}
+
 	// as fallback, get the election and return the landing png
 	election, err := v.election(electionID)
 	if err != nil {
