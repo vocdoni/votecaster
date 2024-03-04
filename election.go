420749871ffc2b4eaec9580d6fac8bd0aa613904
Lucas Menendez <hi@lucasmenendez.me>
2024-03-04 18:01:07 +0100

    initial helper to call shortener API

--- election.go
+++ election.go
@@ -22,6 +22,9 @@ import (
 const (
 	ElectionSourceWebApp = "farcaster.vote"
 	ElectionSourceBot    = "bot"
+	shortenerTimeout     = 10 * time.Second
+	shortenerBase        = "https://frame.vote/%s"
+	shortenerEndpoint    = fmt.Sprintf(shortenerBase, "add/%s")
 )
 
 func (v *vocdoniHandler) election(electionID types.HexBytes) (*api.Election, error) {
@@ -343,4 +346,33 @@ func (v *vocdoniHandler) saveElectionAndProfile(election *api.Election, profile 
 		return fmt.Errorf("failed to update user in database: %w", err)
 	}
 	return nil
+}
+
+// ShortElectionURL returns a shortened version of the election URL. It uses the
+// vocdoni shortener service to shorten the URL. It returns the shortened URL or
+// an error if something went wrong. It uses a timeout of 10 seconds for the
+// request.
+func ShortElectionURL(ctx context.Context, electionURL string) (string, error) {
+	internalCtx, cancel := context.WithTimeout(ctx, shortenerTimeout)
+	defer cancel()
+	endpointURL := fmt.Sprintf(shortenerEndpoint, electionURL)
+	req, err := http.NewRequestWithContext(internalCtx, http.MethodGet, endpointURL, nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to create request: %w", err)
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("failed to send request: %w", err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("error downloading json: %s", res.Status)
+	}
+	shortenerResponse := &struct {
+		Link string `json:"link"`
+	}{}
+	if err := json.NewDecoder(res.Body).Decode(shortenerResponse); err != nil {
+		return "", fmt.Errorf("failed to decode json: %w", err)
+	}
+	return fmt.Sprintf(shortenerBase, shortenerResponse.Link), nil
 }
