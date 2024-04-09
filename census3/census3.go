293335fe33efb6d12109d1eaa03d64486eb8b368
Jordi Pinyana <jordi.pinyana.paga@gmail.com>
2024-04-09 18:42:59 +0200

    census3: client use generics

--- census3/census3.go
+++ census3/census3.go
@@ -42,107 +42,110 @@ func NewClient(endpoint string, bearerToken string) (*Client, error) {
 	return c3, nil
 }
 
-// SupportedChains returns the information of the Census3 endpoint supported chains.
-func (c3 *Client) SupportedChains() ([]c3types.SupportedChain, error) {
+// reqFunc is a function type that encapsulates an API call
+type reqFunc[T any] func() (T, error)
+
+// requestWithRetry handles the retry logic for a request
+func requestWithRetry[T any](fn reqFunc[T]) (T, error) {
+	var result T
 	for i := 0; i < maxRetries; i++ {
-		info, err := c3.Info()
+		result, err := fn()
 		if err != nil {
 			time.Sleep(cooldown)
 			continue
 		}
-		return info.SupportedChains, nil
+		return result, nil
 	}
-	return nil, fmt.Errorf("failed to get info from census3 endpoint")
+	return result, fmt.Errorf("failed after %d retries", maxRetries)
+}
+
+// SupportedChains returns the information of the Census3 endpoint supported chains.
+func (c3 *Client) SupportedChains() ([]c3types.SupportedChain, error) {
+	return requestWithRetry(func() ([]c3types.SupportedChain, error) {
+		info, err := c3.Info()
+		if err != nil {
+			return nil, fmt.Errorf("failed to get supported chains: %w", err)
+		}
+		return info.SupportedChains, nil
+	})
 }
 
 // Tokens returns the list of tokens registered in the Census3 endpoint.
 func (c3 *Client) Tokens() ([]*c3types.TokenListItem, error) {
-	for i := 0; i < maxRetries; i++ {
+	return requestWithRetry(func() ([]*c3types.TokenListItem, error) {
 		tokens, err := c3.GetTokens(-1, "", "")
 		if err != nil {
-			time.Sleep(cooldown)
-			continue
+			return nil, fmt.Errorf("failed to get tokens: %w", err)
 		}
 		return tokens, nil
-	}
-	return nil, fmt.Errorf("failed to get tokens from census3 endpoint")
+
+	})
 }
 
 // Token returns the token with the given ID.
 func (c3 *Client) Token(tokenID, externalID string, chainID uint64) (*c3types.Token, error) {
-	for i := 0; i < maxRetries; i++ {
+	return requestWithRetry(func() (*c3types.Token, error) {
 		token, err := c3.GetToken(tokenID, chainID, externalID)
 		if err != nil {
-			time.Sleep(cooldown)
-			continue
+			return nil, fmt.Errorf("failed to get token: %w", err)
 		}
 		return token, nil
-	}
-	return nil, fmt.Errorf("failed to get token from census3 endpoint")
+	})
 }
 
 // SupportedTokens returns the list of tokens supported by the Census3 endpoint.
 func (c3 *Client) SupportedTokens() ([]string, error) {
-	for i := 0; i < maxRetries; i++ {
+	return requestWithRetry(func() ([]string, error) {
 		tokens, err := c3.GetTokenTypes()
 		if err != nil {
-			time.Sleep(cooldown)
-			continue
+			return nil, fmt.Errorf("failed to get supported tokens: %w", err)
 		}
 		return tokens, nil
-	}
-	return nil, fmt.Errorf("failed to get supported tokens from census3 endpoint")
+	})
 }
 
 // Strategies returns the list of strategies registered in the Census3 endpoint.
 func (c3 *Client) Strategies() ([]*c3types.Strategy, error) {
-	for i := 0; i < maxRetries; i++ {
+	return requestWithRetry(func() ([]*c3types.Strategy, error) {
 		strategies, err := c3.GetStrategies(-1, "", "")
 		if err != nil {
-			time.Sleep(cooldown)
-			continue
+			return nil, fmt.Errorf("failed to get strategies: %w", err)
 		}
 		return strategies, nil
-	}
-	return nil, fmt.Errorf("failed to get strategies from census3 endpoint")
+	})
 }
 
 // Strategy returns the strategy with the given ID.
 func (c3 *Client) Strategy(strategyID uint64) (*c3types.Strategy, error) {
-	for i := 0; i < maxRetries; i++ {
+	return requestWithRetry(func() (*c3types.Strategy, error) {
 		strategy, err := c3.GetStrategy(strategyID)
 		if err != nil {
-			time.Sleep(cooldown)
-			continue
+			return nil, fmt.Errorf("failed to get strategy: %w", err)
 		}
 		return strategy, nil
-	}
-	return nil, fmt.Errorf("failed to get strategy from census3 endpoint")
+	})
 }
 
 // GetStrategyHolders returns the list of holders for the given strategy.
 // The returned map has the holder's address as key and the holder's balance as a string encoded big.Int
 func (c3 *Client) GetStrategyHolders(strategyID uint64) (map[string]string, error) {
-	for i := 0; i < maxRetries; i++ {
+	return requestWithRetry(func() (map[string]string, error) {
 		holders, err := c3.GetHoldersByStrategy(strategyID)
 		if err != nil {
-			time.Sleep(cooldown)
-			continue
+			return nil, fmt.Errorf("failed to get strategy holders: %w", err)
 		}
 		return holders.Holders, nil
-	}
-	return nil, fmt.Errorf("failed to get strategy holders from census3 endpoint")
+	})
 }
 
 // Census returns the census with the given ID.
 func (c3 *Client) Census(censusID uint64) (*c3types.Census, error) {
-	for i := 0; i < maxRetries; i++ {
+	return requestWithRetry(func() (*c3types.Census, error) {
 		census, err := c3.GetCensus(censusID)
 		if err != nil {
-			time.Sleep(cooldown)
-			continue
+			return nil, fmt.Errorf("failed to get census: %w", err)
 		}
 		return census, nil
-	}
-	return nil, fmt.Errorf("failed to get census from census3 endpoint")
+
+	})
 }
