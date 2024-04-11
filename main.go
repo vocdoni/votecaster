ecb69da61610cb343378025f44d894416582a4b0
Lucas Menéndez <hi@lucasmenendez.me>
2024-04-11 15:14:36 +0200

    feature: manage notifications mutes via api (#51)
    * new endpoints to mute and unmute users using their usernames
    
    * change to camelCase the uris of the new endpoints and make them private

--- main.go
+++ main.go
@@ -522,6 +522,14 @@ func main() {
 		log.Fatal(err)
 	}
 
+	if err := uAPI.Endpoint.RegisterMethod("/profile/mutedUsers", http.MethodPost, "private", handler.muteUserHandler); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := uAPI.Endpoint.RegisterMethod("/profile/mutedUsers/{username}", http.MethodDelete, "private", handler.unmuteUserHandler); err != nil {
+		log.Fatal(err)
+	}
+
 	// if a bot FID is provided, start the bot background process
 	if botFid > 0 {
 		var botAPI farcasterapi.API
--- users.go
+++ users.go
@@ -52,3 +52,79 @@ func (v *vocdoniHandler) profileHandler(msg *apirest.APIdata, ctx *httprouter.HT
 	}
 	return ctx.Send(data, apirest.HTTPstatusOK)
 }
+
+func (v *vocdoniHandler) muteUserHandler(msg *apirest.APIdata, ctx *httprouter.HTTPContext) error {
+	// get the authenticated user
+	token := msg.AuthToken
+	if token == "" {
+		return ctx.Send([]byte("missing auth token header"), apirest.HTTPstatusBadRequest)
+	}
+	auth, err := v.db.UpdateActivityAndGetData(token)
+	if err != nil {
+		return ctx.Send([]byte(err.Error()), apirest.HTTPstatusNotFound)
+	}
+	// parse the username from the request
+	req := map[string]string{}
+	if err := json.Unmarshal(msg.Data, &req); err != nil {
+		return ctx.Send([]byte("could not parse request"), apirest.HTTPstatusBadRequest)
+	}
+	usernameToMute, ok := req["username"]
+	if !ok {
+		return ctx.Send([]byte("missing username"), apirest.HTTPstatusBadRequest)
+	}
+	// get the user to mute
+	userToMute, err := v.db.UserByUsername(usernameToMute)
+	if err != nil {
+		return ctx.Send([]byte("user not found"), apirest.HTTPstatusNotFound)
+	}
+	// check if the user is already muted
+	isMuted, err := v.db.IsUserNotificationMuted(auth.UserID, userToMute.UserID)
+	if err != nil {
+		return ctx.Send([]byte("could not check if user is muted"), apirest.HTTPstatusInternalErr)
+	}
+	// if the user is already muted, return an error
+	if isMuted {
+		return ctx.Send([]byte("user is already muted"), apirest.HTTPstatusBadRequest)
+	}
+	// mute the user
+	if err := v.db.AddNotificationMutedUser(auth.UserID, userToMute.UserID); err != nil {
+		return ctx.Send([]byte("could not mute user"), apirest.HTTPstatusInternalErr)
+	}
+	return ctx.Send([]byte("Ok"), apirest.HTTPstatusOK)
+}
+
+func (v *vocdoniHandler) unmuteUserHandler(msg *apirest.APIdata, ctx *httprouter.HTTPContext) error {
+	// get the authenticated user
+	token := msg.AuthToken
+	if token == "" {
+		return ctx.Send([]byte("missing auth token header"), apirest.HTTPstatusBadRequest)
+	}
+	auth, err := v.db.UpdateActivityAndGetData(token)
+	if err != nil {
+		return ctx.Send([]byte(err.Error()), apirest.HTTPstatusNotFound)
+	}
+	// get the muted username from the request
+	mutedUsername := ctx.URLParam("username")
+	if mutedUsername == "" {
+		return ctx.Send([]byte("missing username"), apirest.HTTPstatusBadRequest)
+	}
+	// get the muted user from the database
+	mutedUser, err := v.db.UserByUsername(mutedUsername)
+	if err != nil {
+		return ctx.Send([]byte("user not found"), apirest.HTTPstatusNotFound)
+	}
+	// check if the user is muted
+	isMuted, err := v.db.IsUserNotificationMuted(auth.UserID, mutedUser.UserID)
+	if err != nil {
+		return ctx.Send([]byte("could not check if user is muted"), apirest.HTTPstatusInternalErr)
+	}
+	// if the user is not muted, return an error
+	if !isMuted {
+		return ctx.Send([]byte("user is not muted"), apirest.HTTPstatusBadRequest)
+	}
+	// unmute the user
+	if err := v.db.DelNotificationMutedUser(auth.UserID, mutedUser.UserID); err != nil {
+		return ctx.Send([]byte("could not mute user"), apirest.HTTPstatusInternalErr)
+	}
+	return ctx.Send([]byte("Ok"), apirest.HTTPstatusOK)
+}
