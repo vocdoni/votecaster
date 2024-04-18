b815c837893f2944aa3e548db43cc9ac81f51473
selankon <selankon@selankon.xyz>
2024-04-18 17:05:11 +0200

    Fix ocmmunity creation

--- webapp/src/util/types.ts
+++ webapp/src/util/types.ts
@@ -1,3 +1,5 @@
+import {CensusType} from "../components/CensusTypeSelector.tsx";
+
 export type FetchFunction = (input: RequestInfo, init?: RequestInit) => Promise<Response>
 
 export type Address = {
@@ -35,3 +37,31 @@ export interface HTTPErrorResponse {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const isErrorWithHTTPResponse = (error: any): error is HTTPErrorResponse =>
   error && typeof error === 'object' && 'response' in error && 'data' in error.response
+
+// This enum comes from the contract repo
+enum ContractCensusType {
+  FC,         /// All Farcaster users.
+  CHANNEL,    /// Users in a specific channel.
+  FOLLOWERS,  /// Users following a specific account.
+  CSV,        /// Users in a CSV file.
+  ERC20,      /// Users holding a specific ERC20 token.
+  NFT         /// Users holding a specific NFT.
+}
+
+export const censusTypeToEnum = (census: CensusType) => {
+  switch (census) {
+    case 'channel':
+      return ContractCensusType.CHANNEL
+    case 'followers':
+      return ContractCensusType.FOLLOWERS
+    case 'custom':
+      return ContractCensusType.CSV
+    case 'erc20':
+      return ContractCensusType.ERC20
+    case 'nft':
+      return ContractCensusType.NFT
+    case 'farcaster':
+    default:
+      return ContractCensusType.FC
+  }
+}
