ac4304fb7b4e65ea9d9c8eb41953ba97b348c535
selankon <selankon@selankon.xyz>
2024-04-19 11:14:46 +0200

    Refactor env vars names
    Use
    
    DEGEN_CONTRACT_ADDRESS -> VOCDONI_COMMUNITYHUBADDRESS
    RESULTS_CONTRACT_ADDRESS -> VOCDONI_COMMUNITYRESULTSADDRESS

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -19,9 +19,9 @@ const viteconfig = ({mode}) => {
     },
     define: {
       'import.meta.env.APP_URL': JSON.stringify(process.env.APP_URL || ''),
-      'import.meta.env.DEGEN_CONTRACT_ADDRESS': JSON.stringify(process.env.DEGEN_CONTRACT_ADDRESS || '0xd4768df803c5a9eDA475159cfbBcF9c06F077c13'),
-      'import.meta.env.RESULTS_CONTRACT_ADDRESS': JSON.stringify(
-        process.env.RESULTS_CONTRACT_ADDRESS || '0x56c24fedad2C98C89830A2Ac74e9B70e1E2ca042'
+      'import.meta.env.VOCDONI_COMMUNITYHUBADDRESS': JSON.stringify(process.env.VOCDONI_COMMUNITYHUBADDRESS || '0xd4768df803c5a9eDA475159cfbBcF9c06F077c13'),
+      'import.meta.env.VOCDONI_COMMUNITYRESULTSADDRESS': JSON.stringify(
+        process.env.VOCDONI_COMMUNITYRESULTSADDRESS || '0x56c24fedad2C98C89830A2Ac74e9B70e1E2ca042'
       ),
     },
     plugins: [
