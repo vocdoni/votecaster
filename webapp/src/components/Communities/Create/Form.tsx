9c6b403e1c3244974d48bba6657760c809497832
selankon <selankon@selankon.xyz>
2024-04-19 10:53:50 +0200

    Migrate to hardhat bindings

--- webapp/src/components/Communities/Create/Form.tsx
+++ webapp/src/components/Communities/Create/Form.tsx
@@ -1,7 +1,6 @@
 import {Alert, Box, Heading, Text, VStack} from '@chakra-ui/react'
 import {FormProvider, SubmitHandler, useForm} from 'react-hook-form'
-import {useWriteContract} from 'wagmi'
-import {abi} from '../../../abi.json'
+import {useAccount, useWalletClient, type UseWalletClientReturnType} from 'wagmi'
 import {degenContractAddress, electionResultsContract} from '../../../util/constants'
 import {CensusFormValues} from '../../CensusTypeSelector'
 import {CensusSelector} from './CensusSelector'
@@ -9,8 +8,12 @@ import {Channels} from './Channels'
 import {Confirm} from './Confirm'
 import {CommunityMetaFormValues, Meta} from './Meta'
 import {censusTypeToEnum} from "../../../util/types.ts";
-import {useEffect} from "react";
-
+import {useCallback, useState} from "react";
+import {CommunityHub__factory} from '../../../typechain'
+import {CommunityHubInterface, ICommunityHub} from "../../../typechain/src/CommunityHub.ts";
+import {BrowserProvider} from "ethers";
+import {id} from "@ethersproject/hash";
+import {ContractTransactionReceipt} from "ethers";
 
 export type CommunityFormValues = Pick<CensusFormValues, 'addresses' | 'censusType' | 'channel'> &
   CommunityMetaFormValues & {
@@ -19,30 +22,36 @@ export type CommunityFormValues = Pick<CensusFormValues, 'addresses' | 'censusTy
 
 export const CommunitiesCreateForm = () => {
   const methods = useForm<CommunityFormValues>()
-  const {data: hash, isPending, writeContract, error} = useWriteContract()
+  // const {data: hash, isPending, writeContract, error} = useWriteContract()
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState<string | null>(null)
+  const {data: walletClient} = useWalletClient()
+  const {address} = useAccount()
 
-  const onSubmit: SubmitHandler<CommunityFormValues> = async (data) => {
+  const onSubmit: SubmitHandler<CommunityFormValues> = useCallback(async (data) => {
+    if (isPending) return;
+    setError(null)
     try {
+      setIsPending(true)
       console.info('received form data:', data)
 
+      const metadata: ICommunityHub.CommunityMetadataStruct = {
+        name: data.name, // name
+        imageURI: data.logo, // logo uri
+        groupChatURL: "https://t.me/nothing", // groupChatURL
+        channels: data.channels.map((chan) => chan.value) ?? [],  // channels
+        notifications: false // notifications
+      }
 
-      const metadata = [
-        data.name, // name
-        data.logo, // logo uri
-        "https://t.me/nothing", // groupChatURL
-        data.channels.map((chan) => chan.value) ?? [],  // channels
-        false // notifications
-      ]
-
-      const census = [
-        censusTypeToEnum(data.censusType), // Census type
-        data.addresses?.filter(({_, address}) => address !== '')
-          .map(({blockchain, address}) => [blockchain, address]), // tokens
-        data.channel ?? '' // channel
-      ]
+      const census: ICommunityHub.CensusStruct = {
+        censusType: censusTypeToEnum(data.censusType), // Census type
+        tokens: data.addresses?.filter(({_, address}) => address !== '')
+          .map(({blockchain, address}) => [blockchain, address]) ?? [], // tokens
+        channel: data.channel ?? '' // channel
+      }
 
       const guardians = data.admins.map((admin) => admin.value)
-      const createElectionPermission = false
+      const createElectionPermission = BigInt(0)
 
       console.info('Degen contract address', degenContractAddress)
       console.info('mapped for contract write:', [
@@ -53,24 +62,49 @@ export const CommunitiesCreateForm = () => {
         createElectionPermission,
       ])
 
-      writeContract({
-        address: degenContractAddress,
-        abi,
-        functionName: 'createCommunity',
-        args: [metadata, census, guardians, electionResultsContract, createElectionPermission],
-      })
+      let signer: any
+      if (walletClient && address && walletClient.account.address === address) {
+        signer = await walletClientToSigner(walletClient)
+      }
+      if (!signer) throw Error("Can't get the signer")
+
+      const communityHubContract = CommunityHub__factory.connect(degenContractAddress, signer)
+
+      const tx = await communityHubContract.createCommunity(
+        metadata, census, guardians, electionResultsContract, createElectionPermission)
+
+      const receipt = await tx.wait()
+
+      if (!receipt) {
+        throw Error("Cannot get receipt")
+      }
+
+      const communityHubInterface = CommunityHub__factory.createInterface();
+      const log = findLog(
+        receipt,
+        communityHubInterface,
+      );
+      if (!log) {
+        throw Error("Cannot get community log")
+      }
+      const parsedLog = communityHubInterface.parseLog(log)
+      const communityId = parsedLog?.args['communityId']
+      if (!communityId) {
+        throw Error("Cannot get community id")
+      }
+      console.log("Commnuity id found", communityId, tx.hash)
+
     } catch (e) {
       console.error('could not create community:', e)
+      if (e instanceof Error) {
+        setError('could not create community: ' + e.message)
+      }
+    } finally {
+      setIsPending(false)
+
     }
-  }
+  }, [walletClient, address, isPending])
 
-  useEffect(() => {
-    console.info("TX hash:", hash)
-  }, [hash])
-
-  if (error) {
-    console.error('error creating community:', error)
-  }
 
   return (
     <Box display='flex' flexDir='column' gap={1}>
@@ -98,8 +132,36 @@ export const CommunitiesCreateForm = () => {
             <Confirm isLoading={isPending}/>
           </Box>
         </Box>
-        {error && <Alert status='error'>{error.message}</Alert>}
+        {error && <Alert status='error'>{error}</Alert>}
       </FormProvider>
     </Box>
   )
+}
+
+export async function walletClientToSigner(walletClient: UseWalletClientReturnType['data']) {
+  const {account, chain, transport} = walletClient!
+  const network = {
+    chainId: chain.id,
+    name: chain.name,
+    ensAddress: chain.contracts?.ensRegistry?.address,
+  }
+  const provider = new BrowserProvider(transport, network)
+  const signer = await provider.getSigner(account.address)
+  return signer
+}
+
+export function findLog(
+  receipt: ContractTransactionReceipt,
+  iface: CommunityHubInterface,
+) {
+  return receipt.logs.find(
+    (log) => {
+      return log.topics[0] ===
+        id(
+          iface.getEvent("CommunityCreated").format(
+            "sighash",
+          ),
+        );
+    }
+  );
 }
