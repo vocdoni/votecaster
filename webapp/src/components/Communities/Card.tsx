8fa76110c32c317e32289030688ed3adec6faf11
selankon <selankon@selankon.xyz>
2024-04-19 13:05:29 +0200

    Implement admin badge

--- webapp/src/components/Communities/Card.tsx
+++ webapp/src/components/Communities/Card.tsx
@@ -1,14 +1,21 @@
-import { Avatar, HStack, Link, LinkProps, Text } from '@chakra-ui/react'
-import { Link as RouterLink } from 'react-router-dom'
+import {Avatar, Badge, Flex, HStack, Link, LinkProps, Text} from '@chakra-ui/react'
+import {Link as RouterLink} from 'react-router-dom'
+import {useAuth} from "../Auth/useAuth.ts";
+import {Community} from "../../queries/communities.ts";
 
 type CommunityCardProps = LinkProps & {
-  name: string
-  slug?: string
-  pfpUrl: string
+  community: Community
 }
 
-export const CommunityCard = ({ name, slug, pfpUrl }: CommunityCardProps) => (
-  <Link
+export const CommunityCard = ({community}: CommunityCardProps) => {
+  const {profile} = useAuth()
+  const adminsFid = community.admins.map((admin) => admin.fid)
+  const isAdmin = profile && adminsFid.includes(profile.fid)
+  const slug = community.id
+  const pfpUrl = community.logoURL
+  const name = community.name
+
+  return <Link
     as={RouterLink}
     to={slug ? `/communities/${slug}` : undefined}
     w='full'
@@ -18,11 +25,17 @@ export const CommunityCard = ({ name, slug, pfpUrl }: CommunityCardProps) => (
     boxShadow='sm'
     borderRadius='lg'
     bg='white'
-    _hover={{ boxShadow: 'none', bg: 'purple.100' }}
+    _hover={{boxShadow: 'none', bg: 'purple.100'}}
   >
     <HStack>
-      <Avatar src={pfpUrl} />
-      <Text fontWeight='500'>{name}</Text>
+      <Avatar src={pfpUrl}/>
+      <Flex mx={2} w={'full'} justifyItems={'start'} alignItems={'start'} justifyContent={'space-between'}>
+        <Text fontWeight='bold'>
+          {name}
+        </Text>
+        {isAdmin && <Badge ml='1' colorScheme='green'>Admin</Badge>}
+
+      </Flex>
     </HStack>
   </Link>
-)
+}
--- webapp/src/components/Communities/index.tsx
+++ webapp/src/components/Communities/index.tsx
@@ -1,15 +1,15 @@
-import { Box, Button, Heading, Link, SimpleGrid, Text, VStack } from '@chakra-ui/react'
-import { useQuery } from '@tanstack/react-query'
-import { MdOutlineGroupAdd } from 'react-icons/md'
-import { Link as RouterLink } from 'react-router-dom'
-import { fetchCommunities } from '../../queries/communities'
-import { useAuth } from '../Auth/useAuth'
-import { Check } from '../Check'
-import { CommunityCard } from './Card'
+import {Box, Button, Heading, Link, SimpleGrid, Text, VStack} from '@chakra-ui/react'
+import {useQuery} from '@tanstack/react-query'
+import {MdOutlineGroupAdd} from 'react-icons/md'
+import {Link as RouterLink} from 'react-router-dom'
+import {fetchCommunities} from '../../queries/communities'
+import {useAuth} from '../Auth/useAuth'
+import {Check} from '../Check'
+import {CommunityCard} from './Card'
 
 export const CommunitiesList = () => {
-  const { bfetch } = useAuth()
-  const { data, error, isLoading } = useQuery({
+  const {bfetch} = useAuth()
+  const {data, error, isLoading} = useQuery({
     queryKey: ['communities'],
     queryFn: fetchCommunities(bfetch),
   })
@@ -17,13 +17,13 @@ export const CommunitiesList = () => {
   return (
     <VStack spacing={4} w='full' alignItems='start'>
       <Heading size='md'>Communities</Heading>
-      <SimpleGrid gap={4} w='full' alignItems='start' columns={{ base: 1, md: 2, lg: 4 }}>
+      <SimpleGrid gap={4} w='full' alignItems='start' columns={{base: 1, md: 2, lg: 4}}>
         {data &&
           data.map((community, k) => (
-            <CommunityCard name={community.name} slug={community.id} key={k} pfpUrl={community.logoURL} />
+            <CommunityCard key={k} community={community}/>
           ))}
       </SimpleGrid>
-      <Check error={error} isLoading={isLoading} />
+      <Check error={error} isLoading={isLoading}/>
       <Box
         w='full'
         boxShadow='sm'
@@ -42,7 +42,7 @@ export const CommunitiesList = () => {
           Create your own community and start managing its governance
         </Text>
         <Link as={RouterLink} to='/communities/new'>
-          <Button leftIcon={<MdOutlineGroupAdd />}>Create a community</Button>
+          <Button leftIcon={<MdOutlineGroupAdd/>}>Create a community</Button>
         </Link>
       </Box>
     </VStack>
