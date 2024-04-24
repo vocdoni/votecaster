517e677577c8587ac9d3de4d622e666283a213e6
Lucas Menendez <hi@lucasmenendez.me>
2024-04-25 00:37:16 +0200

    fixing navigation issue

--- webapp/src/pages/communities/view.tsx
+++ webapp/src/pages/communities/view.tsx
@@ -4,20 +4,21 @@ import { useAuth } from '../../components/Auth/useAuth'
 import { Check } from '../../components/Check'
 import { CommunitiesView } from '../../components/Communities/View'
 import { fetchCommunity } from '../../queries/communities'
+import { Text } from '@chakra-ui/react'
 
 const Community = () => {
   const { id } = useParams()
   const { bfetch } = useAuth()
-  const { data, isLoading, error } = useQuery({
-    queryKey: ['community'],
+  const { data: community, isLoading, error } = useQuery({
+    queryKey: ['community', id],
     queryFn: fetchCommunity(bfetch, id),
+    enabled: !!id,
   })
 
   if (isLoading || error) {
     return <Check isLoading={isLoading} error={error} />
   }
-
-  return <CommunitiesView community={data} />
+  return <CommunitiesView community={community} />
 }
 
 export default Community
