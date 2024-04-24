f5d92fc7265509c62556aa23aa64a2b9d7eb1e2c
Lucas Menéndez <hi@lucasmenendez.me>
2024-04-24 09:48:50 +0200

    including icon on channel selector

--- webapp/src/components/Communities/Create/Channels.tsx
+++ webapp/src/components/Communities/Create/Channels.tsx
@@ -1,9 +1,29 @@
-import {FormControl, FormErrorMessage, FormLabel, Heading, Text} from '@chakra-ui/react'
+import {FormControl, FormErrorMessage, FormLabel, Heading, Image, Box, Text} from '@chakra-ui/react'
+import { components } from 'react-select';
 import {AsyncSelect} from 'chakra-react-select'
 import {useState} from 'react'
 import {Controller, useFormContext} from 'react-hook-form'
 import {fetchChannelQuery} from '../../../queries/channels'
 import {useAuth} from '../../Auth/useAuth'
+
+// CustomOption Component
+const CustomOption = (props) => {
+  return (
+    <components.Option {...props}>
+      <Box display="flex" alignItems="center">
+        <Image
+          src={props.data.image}  // Image URL from the option data
+          borderRadius="full"     // Makes the image circular
+          boxSize="20px"          // Sets the size of the image
+          objectFit="cover"       // Ensures the image covers the area properly
+          mr="8px"                // Right margin for spacing
+          alt={props.data.label}  // Alt text for accessibility
+        />
+        <Text>{props.data.label}</Text>
+      </Box>
+    </components.Option>
+  );
+};
 
 export type ChannelsFormValues = {
   channels: { label: string; value: string }[]
@@ -34,11 +54,13 @@ export const Channels = () => {
             noOptionsMessage={() => 'No channels found'}
             placeholder='Search and add channels'
             {...field}
+            components={{Option: CustomOption}}
             loadOptions={async (inputValue) => {
               try {
                 setLoading(true)
                 return (await fetchChannelQuery(bfetch)(inputValue)).map((channel) => ({
                   label: channel.name,
+                  image: channel.image,
                   value: channel.id,
                 }))
               } catch (e) {
