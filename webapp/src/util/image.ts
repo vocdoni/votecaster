01bb61060f172d4b1f5b744ada65af96fbcf3c81
Òscar Casajuana <elboletaire@gmail.com>
2024-04-29 15:50:30 +0200

    Communities logo uploader & cropper

---
+++ webapp/src/util/image.ts
@@ -0,0 +1,26 @@
+import { Crop } from 'react-image-crop'
+
+export const drawImage = (image: HTMLImageElement, crop: Crop) => {
+  const canvas = document.createElement('canvas')
+  const scaleX = image.naturalWidth / image.width
+  const scaleY = image.naturalHeight / image.height
+  canvas.width = crop.width
+  canvas.height = crop.height
+  const ctx = canvas.getContext('2d')
+
+  // New coordinates for the cropped area
+  ctx.drawImage(
+    image,
+    crop.x * scaleX,
+    crop.y * scaleY,
+    crop.width * scaleX,
+    crop.height * scaleY,
+    0,
+    0,
+    crop.width,
+    crop.height
+  )
+
+  const croppedDataUrl = canvas.toDataURL('image/jpeg')
+  return croppedDataUrl
+}
