66d08424ea43ba7c84dd2fe33f6b8fab4b86ba11
Òscar Casajuana <elboletaire@gmail.com>
2024-05-02 16:30:50 +0200

    Minor type fix in vite.config.ts

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,11 +1,11 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv, UserConfigFn } from 'vite'
 import { createHtmlPlugin } from 'vite-plugin-html'
 import svgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
-const viteconfig = ({ mode }) => {
+const viteconfig: UserConfigFn = ({ mode }) => {
   // load env variables from .env files
   process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') }
 
