805d2fec7cf215753520aa18fe42fe351f99e870
Òscar Casajuana <elboletaire@gmail.com>
2024-05-07 10:39:12 +0200

    Change purple.500 to #855DCD
    closes #112

--- webapp/src/theme.ts
+++ webapp/src/theme.ts
@@ -1,6 +1,11 @@
 import { defineStyleConfig, extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
+  colors: {
+    purple: {
+      500: '#855DCD',
+    },
+  },
   fonts: {
     heading: '"Inter", sans-serif',
     body: '"Inter", sans-serif',
