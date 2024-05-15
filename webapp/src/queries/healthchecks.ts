d697002995acfbcc6c908a55c91aca77e0a1895e
Òscar Casajuana <elboletaire@gmail.com>
2024-05-15 09:15:20 +0200

    Fix healthcheck call

--- webapp/src/queries/healthchecks.ts
+++ webapp/src/queries/healthchecks.ts
@@ -1,7 +1,15 @@
+import { degen } from 'wagmi/chains'
 import { degenChainRpc } from '~constants'
 
 export const degenHealth = async (): Promise<boolean> => {
-  const response = await fetch(degenChainRpc)
+  const response = await fetch(degenChainRpc, {
+    method: 'POST',
+    body: JSON.stringify({
+      method: 'eth_blockNumber',
+      id: degen.id,
+      jsonrpc: '2.0',
+    }),
+  })
   if (!response.ok) {
     throw new Error('Degen RPC health check failed')
   }
