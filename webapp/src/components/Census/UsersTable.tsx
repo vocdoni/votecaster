00167645dc1003f7573a3aa239e3c2bf630d51d5
Lucas Menéndez <hi@lucasmenendez.me>
2024-05-23 01:49:35 +0200

    selectable user table

--- webapp/src/components/Census/UsersTable.tsx
+++ webapp/src/components/Census/UsersTable.tsx
@@ -1,10 +1,38 @@
-import { Table, TableProps, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
+import { Checkbox, Table, TableProps, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
+import { useState } from 'react';
 
-export const UsersTable = ({ users, ...props }: { users?: string[][] } & TableProps) => {
+interface UsersTableProps extends TableProps {
+  users?: string[][];
+  selectable?: boolean;
+  hasWeight?: boolean;
+  onSelectionChange?: (selected: string[][]) => void;
+}
+
+export const UsersTable = ({ users, selectable, onSelectionChange, hasWeight, ...props }: UsersTableProps) => {
+  const [selectedUsers, setSelectedUsers] = useState<string[][]>([]);
+
   if (!users || !users.length) return
 
-  // check if table has weight column
-  const hasWeight = users[0].length > 1
+  // check if table has weight column only if has the hasWeight prop is not defined
+  if (hasWeight === undefined) {
+    hasWeight = users[0].length > 1
+  }
+
+  const handleCheckboxChange = (username: string, weight: string, isChecked: boolean) => {
+    const data = [username]
+    if (weight) {
+      data.push(weight)
+    }
+    const updatedSelectedUsers = isChecked
+      ? [...selectedUsers, data]
+      : selectedUsers.filter(user => user[0] !== username);
+
+    setSelectedUsers(updatedSelectedUsers);
+
+    if (onSelectionChange) {
+      onSelectionChange(updatedSelectedUsers);
+    }
+  };
 
   return (
     <Table {...props}>
@@ -17,8 +45,16 @@ export const UsersTable = ({ users, ...props }: { users?: string[][] } & TablePr
       <Tbody>
         {users.map(([username, weight]) => (
           <Tr key={username}>
-            <Td>{username}</Td>
-            {!!weight && <Td>{weight}</Td>}
+            <Td>
+              {selectable && (
+                <Checkbox
+                  pr={3}
+                  onChange={(e) => handleCheckboxChange(username, weight, e.target.checked)}
+                />
+              )}
+              {username}
+            </Td>
+            {hasWeight && !!weight  && <Td>{weight}</Td>}
           </Tr>
         ))}
       </Tbody>
