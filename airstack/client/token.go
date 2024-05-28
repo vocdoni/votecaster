42b577767f1f0cf4605051e441d06191b805c9c7
Jordi Pinyana <jordi.pinyana.paga@gmail.com>
2024-05-28 18:55:37 +0200

    Add support for more Airstack queries:
    - Get farcaster follower count given a fid
    - Check if a given user follows a another given user (using FIDs)
    - Check if an EVM address holds a given token in a given chain

--- airstack/client/token.go
+++ airstack/client/token.go
@@ -120,3 +120,40 @@ func (c *Client) TokenBalances(tokenAddress common.Address, blockchain string) (
 	log.Debugf("fetched %d items in %d pages for token %s", totalHolders, totalPages, tokenAddress)
 	return th, nil
 }
+
+// CheckIfHolder checks if a given address is a holder of a given token in a given chain
+// Returns the balance of the token if the address is a holder.
+func (c *Client) CheckIfHolder(tokenAddress common.Address, blockchain string, address common.Address) (uint64, error) {
+	chain, ok := c.blockchainToTokenBlockchain(blockchain)
+	if !ok {
+		return 0, fmt.Errorf("invalid blockchain provided")
+	}
+	resp, err := c.holderOf(tokenAddress, chain, address)
+	if err != nil {
+		return 0, fmt.Errorf("cannot check token ownership from Airstack: %w", err)
+	}
+	return uint64(resp.TokenBalances.TokenBalance[0].FormattedAmount), nil
+}
+
+// holderOf is a wrapper around the generated function for GraphQL query checkTokenOwnership.
+func (c *Client) holderOf(
+	tokenAddress common.Address,
+	blockchain gql.TokenBlockchain,
+	address common.Address,
+) (*gql.CheckTokenOwnershipResponse, error) {
+	cctx, cancel := context.WithTimeout(c.ctx, apiTimeout)
+	defer cancel()
+	r := 0
+	var err error
+	resp := &gql.CheckTokenOwnershipResponse{}
+	for r < maxAPIRetries {
+		resp, err = gql.CheckTokenOwnership(cctx, c.Client, address.Hex(), tokenAddress, blockchain)
+		if err != nil {
+			r += 1
+			time.Sleep(time.Second * 3)
+			continue
+		}
+		return resp, nil
+	}
+	return nil, fmt.Errorf("max GraphQL retries reached, error: %w", err)
+}
