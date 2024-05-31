fd0f75b8778b6ff61d45807d1bb58692278700c6
Lucas Menéndez <hi@lucasmenendez.me>
2024-05-31 09:10:07 +0200

    fixing unique usernames count during election creation

--- election.go
+++ election.go
@@ -115,7 +115,11 @@ func (v *vocdoniHandler) createElection(msg *apirest.APIdata, ctx *httprouter.HT
 		}
 	}
 	// create the election description
-	req.ElectionDescription.UsersCount = uint32(len(census.Usernames))
+	uniqueUsernames := map[string]bool{}
+	for _, u := range census.Usernames {
+		uniqueUsernames[u] = true
+	}
+	req.ElectionDescription.UsersCount = uint32(len(uniqueUsernames))
 	req.ElectionDescription.UsersCountInitial = uint32(census.FromTotalAddresses)
 	// create the election and save it in the database
 	electionID, err := v.createAndSaveElectionAndProfile(&req.ElectionDescription, census,
