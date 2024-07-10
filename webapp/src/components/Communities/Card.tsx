f74fa16647b5dc50d5d4529809a2057993e72329
Òscar Casajuana <elboletaire@gmail.com>
2024-07-10 17:02:31 +0200

    Fix logo preview showing wrong chain logo
    closes #155

--- webapp/src/components/Communities/Card.tsx
+++ webapp/src/components/Communities/Card.tsx
@@ -1,7 +1,8 @@
-import { Avatar, Badge, Box, Flex, HStack, Link, LinkProps, Text, VStack } from '@chakra-ui/react'
+import { Avatar, Badge, Box, Flex, HStack, Link, LinkProps, StackProps, Text, VStack } from '@chakra-ui/react'
 import { Link as RouterLink } from 'react-router-dom'
+import { useAccount } from 'wagmi'
 import { useAuth } from '~components/Auth/useAuth'
-import { getChain } from '~util/chain'
+import { chainAlias, getChain } from '~util/chain'
 import { chainFromId, numberFromId } from '~util/mappings'
 
 type CommunityCardProps = LinkProps & {
@@ -12,48 +13,31 @@ type CommunityCardProps = LinkProps & {
   disabled?: boolean
 }
 export const CommunityCard = ({ name, id, pfpUrl, admins, disabled, ...props }: CommunityCardProps) => {
-  const { profile } = useAuth()
-  const adminsFid = admins?.map((admin) => admin.fid) ?? []
-  const isAdmin = profile && adminsFid.includes(profile.fid)
-  const alias = chainFromId(id)
-  const nid = numberFromId(id)
+  const style = {
+    w: 'full',
+    border: '1px solid',
+    borderColor: 'gray.200',
+    p: 2,
+    boxShadow: 'sm',
+    borderRadius: 'lg',
+    bg: 'white',
+    _hover: { boxShadow: 'none', bg: 'purple.100' },
+    color: disabled ? 'gray.400' : 'black',
+    ...props,
+  }
+
+  if (id) {
+    return (
+      <Link as={RouterLink} to={id ? `/communities/${chainFromId(id)}/${numberFromId(id)}` : undefined} {...style}>
+        <CommunityInfo name={name} id={id} pfpUrl={pfpUrl} admins={admins} disabled={disabled} />
+      </Link>
+    )
+  }
 
   return (
-    <Link
-      as={RouterLink}
-      to={id ? `/communities/${alias}/${nid}` : undefined}
-      w='full'
-      border='1px solid'
-      borderColor='gray.200'
-      p={2}
-      boxShadow='sm'
-      borderRadius='lg'
-      bg='white'
-      _hover={{ boxShadow: 'none', bg: 'purple.100' }}
-      color={disabled ? 'gray.400' : 'black'}
-      {...props}
-    >
-      <HStack>
-        <CommunityLogo disabled={disabled} pfpUrl={pfpUrl} alias={alias} />
-        <Flex
-          mx={2}
-          w={'full'}
-          justifyItems={'start'}
-          alignItems={'center'}
-          justifyContent={'space-between'}
-          flexWrap={'wrap'}
-        >
-          <Text fontWeight='bold' noOfLines={1} wordBreak='break-all'>
-            {name}
-          </Text>
-
-          <VStack>
-            {isAdmin && <Badge colorScheme='green'>Admin</Badge>}
-            {disabled && <Text fontSize={'xs'}>Disabled</Text>}
-          </VStack>
-        </Flex>
-      </HStack>
-    </Link>
+    <Box {...style}>
+      <CommunityInfo name={name} pfpUrl={pfpUrl} admins={admins} disabled={disabled} />
+    </Box>
   )
 }
 
@@ -69,5 +53,44 @@ export const CommunityLogo = ({ alias, pfpUrl, disabled }: CommunityLogoProps) =
       <Avatar src={pfpUrl} filter={disabled ? 'grayscale(1)' : ''} />
       <Avatar pos='absolute' bottom={0} right={0} src={chain.logo} width={4} height={4} />
     </Box>
+  )
+}
+
+type CommunityInfoProps = StackProps & {
+  name: string
+  id?: CommunityID
+  pfpUrl: string
+  admins?: Profile[]
+  disabled?: boolean
+}
+
+const CommunityInfo = ({ name, id, pfpUrl, admins, disabled, ...props }: CommunityInfoProps) => {
+  const { profile } = useAuth()
+  const { chain } = useAccount()
+  const adminsFid = admins?.map((admin) => admin.fid) ?? []
+  const isAdmin = profile && adminsFid.includes(profile.fid)
+  const alias = id ? chainFromId(id) : chainAlias(chain)
+
+  return (
+    <HStack {...props}>
+      <CommunityLogo disabled={disabled} pfpUrl={pfpUrl} alias={alias} />
+      <Flex
+        mx={2}
+        w={'full'}
+        justifyItems={'start'}
+        alignItems={'center'}
+        justifyContent={'space-between'}
+        flexWrap={'wrap'}
+      >
+        <Text fontWeight='bold' noOfLines={1} wordBreak='break-all'>
+          {name}
+        </Text>
+
+        <VStack>
+          {isAdmin && <Badge colorScheme='green'>Admin</Badge>}
+          {disabled && <Text fontSize={'xs'}>Disabled</Text>}
+        </VStack>
+      </Flex>
+    </HStack>
   )
 }
