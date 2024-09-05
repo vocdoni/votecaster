a3dc01b16e8365325115d6fc58473eeea8564605
Òscar Casajuana <elboletaire@gmail.com>
2024-09-05 11:20:44 +0200

    Remove trailing dots
    closes #190

--- webapp/src/components/Home/Jumbotron.tsx
+++ webapp/src/components/Home/Jumbotron.tsx
@@ -7,14 +7,14 @@ export const Jumbotron = () => (
   <Box boxShadow='md' bg='white' borderRadius='md' py={20} px={2} textAlign='center' w='full'>
     <VStack spacing={4} alignItems='center'>
       <Heading as='h1' size='jumbo' fontWeight='800' maxW='800px' display='block'>
-        The governance platform for your Farcaster community.
+        The governance platform for your Farcaster community
       </Heading>
       <Text fontWeight='500' fontSize='xl' color='gray.600'>
-        Get started today.
+        Get started today
       </Text>
       <CreateFarcasterCommunityButton />
       <Text fontStyle='italic' color='gray.400'>
-        Create a community to unlock more census options and enhanced governance features.
+        Create a community to unlock more census options and enhanced governance features
         <br />
         <Link as={RouterLink} variant='primary' to='#features'>
           Check all the Features
